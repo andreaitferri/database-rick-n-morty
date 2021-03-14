abb3092e3497c1566029b0ad639b32e68adb0c4e
andrea <andreaitferri@gmail.com>
2021-03-14 23:07:57 +0100

    http implementation

--- src/App.js
+++ src/App.js
@@ -3,23 +3,49 @@ import './App.css';
 import React from 'react';
 import Header from "./Components/Header/Header";
 import Grid from "./Components/Grid/Grid";
-import Character from "./Components/Card/Character";
+
+/*
+function timeout(ms){
+  return new Promise(resolve => setTimeout(resolve,ms));
+}
+*/
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      charatchers: [Character]
+      isLoading: false,
+      charatchers: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchCharacters();
+  }
+
+
+  fetchCharacters = async () => {
+
+    try{
+        this.setState({isLoading: true});
+        const response = await fetch("https://rickandmortyapi.com/api/character/?page=1");
+        const json = await response.json();
+        this.setState({charatchers: json.results});
+    }catch(e){
+        console.log(e);
+    }finally{
+      this.setState({isLoading:false});
     }
   }
 
 
   render() {
+    const {charatchers, isLoading} = this.state;
     return(
     <div className="App">
         <Header />
-        <Grid charatchers = {this.state.charatchers} />
+        <Grid charatchers = {charatchers} isLoading={isLoading} />
     </div>
     );
     }
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -4,18 +4,24 @@ import Card from "../Card/Card";
 
 import "./Grid.css";
 
-function Grid({ charatchers }){
+function Grid({ charatchers, isLoading}){
     return (
        <div className="container grid">
            {charatchers.map((character) => (
                <Card key={character.id} character={character} />
            ))}
+           {isLoading && (
+                <div className="loading">
+                     <p>loading...</p>
+                </div>
+           )}
        </div>
     );
 }
 
 Grid.protoType = {
     charatchers: PropTypes.array.isRequired,
+    isLoading: PropTypes.bool.isRequired
 };
 
 
