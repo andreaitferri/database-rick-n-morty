a2ff7e1e4829262d76f449f213ae3fe372d92c38
andrea <andreaitferri@gmail.com>
2021-03-14 22:23:04 +0100

    pre ajax call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Component } from 'react';
 import './App.css';
+import React from 'react';
 import Header from "./Components/Header/Header";
 import Grid from "./Components/Grid/Grid";
+import Character from "./Components/Card/Character";
 
-function App() {
-  return (
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      charatchers: [Character]
+    }
+  }
+
+
+  render() {
+    return(
     <div className="App">
         <Header />
-        <Grid />
+        <Grid charatchers = {this.state.charatchers} />
     </div>
-  );
+    );
+    }
 }
 
 export default App;
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,20 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Card from "../Card/Card";
+
 import "./Grid.css";
 
-
-function Grid(){
+function Grid({ charatchers }){
     return (
        <div className="container grid">
-           <Card />
-           <Card />
-           <Card />
-           <Card />
-           <Card />
-           <Card />
-           <Card />
+           {charatchers.map((character) => (
+               <Card key={character.id} character={character} />
+           ))}
        </div>
     );
 }
 
-export default Grid;
+Grid.protoType = {
+    charatchers: PropTypes.array.isRequired,
+};
+
+
+
+
+export default Grid;        
